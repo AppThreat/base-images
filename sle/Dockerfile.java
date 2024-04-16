76125fec2bf94139ccf32def2103068593e33ab3
Prabhu Subramanian <prabhu@appthreat.com>
2024-04-16 11:32:28 +0100

    Added legacy gradle and maven
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- sle/Dockerfile.java
+++ sle/Dockerfile.java
@@ -1,5 +1,17 @@
 FROM registry.suse.com/bci/openjdk-devel:11 as builder
 
+ARG SBT_VERSION=1.8.3
+ARG MAVEN_VERSION=3.6.3
+ARG GRADLE_VERSION=7.6.4
+
+ENV SBT_VERSION=$SBT_VERSION \
+    MAVEN_VERSION=$MAVEN_VERSION \
+    GRADLE_VERSION=$GRADLE_VERSION \
+    GRADLE_OPTS="-Dorg.gradle.daemon=false" \
+    MAVEN_HOME="/opt/maven/${MAVEN_VERSION}" \
+    GRADLE_HOME="/opt/gradle/${GRADLE_VERSION}" \
+    SBT_HOME="/opt/sbt/${SBT_VERSION}"
+
 RUN set -e; \
     ARCH_NAME="$(rpm --eval '%{_arch}')"; \
     url=; \
@@ -15,6 +27,15 @@ RUN set -e; \
         *) echo >&2 "error: unsupported architecture: '$ARCH_NAME'"; exit 1 ;; \
     esac; \
     zypper refresh && zypper --non-interactive update && zypper --non-interactive install -l --no-recommends git-core nodejs20 npm20 python311 python311-pip wget zip unzip make awk \
+    && curl -s "https://get.sdkman.io" | bash \
+    && source "$HOME/.sdkman/bin/sdkman-init.sh" \
+    && echo -e "sdkman_auto_answer=true\nsdkman_selfupdate_feature=false\nsdkman_auto_env=true\nsdkman_curl_connect_timeout=20\nsdkman_curl_max_time=0" >> $HOME/.sdkman/etc/config \
+    && sdk install maven $MAVEN_VERSION \
+    && sdk install gradle $GRADLE_VERSION \
+    && sdk install sbt $SBT_VERSION \
+    && sdk offline enable \
+    && mv /root/.sdkman/candidates/* /opt/ \
+    && rm -rf /root/.sdkman \
     && zypper clean -a
 
 CMD /bin/bash
