2d30a7f4f4bb6b46a2c0a0450e98042437226e8f
Prabhu Subramanian <prabhu@appthreat.com>
2024-09-24 21:45:57 +0100

    Added java slim images
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- cdxgen/Dockerfile.java
+++ cdxgen/Dockerfile.java
@@ -8,7 +8,7 @@ LABEL maintainer="AppThreat" \
       org.opencontainers.image.vendor="AppThreat" \
       org.opencontainers.image.licenses="Apache-2.0" \
       org.opencontainers.image.title="cdxgen" \
-      org.opencontainers.image.description="Rolling image with cdxgen SBOM generator for Java 11 apps" \
+      org.opencontainers.image.description="Rolling image with cdxgen SBOM generator for Java 11 and android apps" \
       org.opencontainers.docker.cmd="docker run --rm -v /tmp:/tmp -p 9090:9090 -v $(pwd):/app:rw -t ghcr.io/appthreat/cdxgen-java:v10 -r /app --server"
 
 ARG CDXGEN_VERSION=10.10.1
